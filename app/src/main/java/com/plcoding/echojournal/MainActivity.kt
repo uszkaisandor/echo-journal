4e58e6d9896462646ee4bc912fef2eebfe0d2976
Sándor Uszkai <uszkaisanyi@gmail.com>
2025-07-15 15:59:05 +0200

    Add base design system

--- app/src/main/java/com/plcoding/echojournal/MainActivity.kt
+++ app/src/main/java/com/plcoding/echojournal/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.plcoding.echojournal.ui.theme.EchoJournalTheme
+import com.plcoding.echojournal.core.presentation.designsystem.theme.EchoJournalTheme
+import com.plcoding.echojournal.core.presentation.designsystem.theme.bgGradient
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +23,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EchoJournalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    containerColor = Color.Transparent,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.bgGradient)
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
